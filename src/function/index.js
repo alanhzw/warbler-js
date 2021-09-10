7821893adde2527ea0a0d40e5cd9202f3d2df7bd
韩志伟 <hanzhiwei@lucidata.cn>
2021-09-10 17:20:12 +0800

    添加模拟延迟的方法

--- src/function/index.js
+++ src/function/index.js
@@ -2,6 +2,13 @@
  * @Author: 一尾流莺
  * @Description:通用函数
  * @Date: 2021-09-10 11:44:18
- * @LastEditTime: 2021-09-10 11:44:18
+ * @LastEditTime: 2021-09-10 17:19:22
  * @FilePath: \warblerJS\src\function\index.js
  */
+
+// 模拟延迟
+import imitateDelay from './imitateDelay';
+
+export {
+  imitateDelay,
+};
