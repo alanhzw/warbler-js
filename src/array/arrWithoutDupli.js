0ccb3aaa11abb13cf3b0fb775b871080694bcd32
韩志伟 <hanzhiwei@lucidata.cn>
2021-09-10 17:45:20 +0800

    添加删除数组中的重复项方法

--- src/array/arrWithoutDupli.js
+++ src/array/arrWithoutDupli.js
@@ -1,17 +1,15 @@
 /*
  * @Author: 一尾流莺
- * @Description:删除数组中的重复项
+ * @Description:删除数组中的重复项(不会修改原数组)
  * @Date: 2021-09-10 11:15:33
- * @LastEditTime: 2021-09-10 17:39:56
+ * @LastEditTime: 2021-09-10 17:44:38
  * @FilePath: \warblerJS\src\array\arrWithoutDupli.js
  */
 
 /**
  * @description:删除数组中的重复项
- * @param { Array } arr 待查询的目标数组
- * @param { String } key 查询对象的键
- * @param { String } value 查询对象的值
- * @return { obj } 查找到的结果
+ * @param { Array } arr 目标去重的数组
+ * @return { Array } 数组去重后的结果
  */
 const arrWithoutDupli = (arr) => [...new Set(arr)];
 
