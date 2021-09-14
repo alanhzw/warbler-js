a22922f27e17b0b3ca39859486a270e5d135575c
韩志伟 <hanzhiwei@lucidata.cn>
2021-09-14 17:06:42 +0800

    修改部分注释

--- src/date/dayOfYear.js
+++ src/date/dayOfYear.js
@@ -2,14 +2,14 @@
  * @Author: 一尾流莺
  * @Description:获取指定日期是所在年份的第几天
  * @Date: 2021-09-14 16:09:42
- * @LastEditTime: 2021-09-14 16:31:03
+ * @LastEditTime: 2021-09-14 16:55:53
  * @FilePath: \warblerJS\src\date\dayOfYear.js
  */
 
 /**
  * @description: 获取指定日期是所在年份的第几天
  * @param { String } date 指定日期 可传参数同 new Date()
- * @return { Number } 指定日期是所在年份天数
+ * @return { Number } 指定日期所在年份的天数
  */
 const dayOfYear = (date) => {
   const myData = date ? new Date(date) : new Date();
@@ -19,9 +19,9 @@ const dayOfYear = (date) => {
 /** 用法
 
 const result1 = dayOfYear()
-const result2 = dayOfYear("2021,9,14")
-console.log(result1)
-console.log(result2)
+const result2 = dayOfYear("2021,9,15")
+console.log(result1) //=> 257
+console.log(result2) //=> 258
 
  */
 
