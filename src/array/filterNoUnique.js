0dee1a89cad8fbe8fd8c7a02795a0c24877d0bec
韩志伟 <hanzhiwei@lucidata.cn>
2021-09-15 11:16:30 +0800

    新增方法

--- src/array/filterNoUnique.js
+++ src/array/filterNoUnique.js
@@ -2,7 +2,7 @@
  * @Author: 一尾流莺
  * @Description:过滤数组中的非唯一值
  * @Date: 2021-09-15 10:54:18
- * @LastEditTime: 2021-09-15 11:11:23
+ * @LastEditTime: 2021-09-15 11:13:02
  * @FilePath: \warblerJS\src\array\filterNoUnique.js
  */
 
@@ -16,7 +16,7 @@ const filterNoUnique = (arr) => arr.filter((i) => arr.indexOf(i) === arr.lastInd
 /** 用法
  const arr = [1,2,2,3,3,4,5,5,6]
  const result = filterNoUnique(arr)
- console.log(result) => [1,4,6]
+ console.log(result) //=> [1,4,6]
  */
 
 export default filterNoUnique;
--- src/array/filterUnique.js
+++ src/array/filterUnique.js
@@ -2,7 +2,7 @@
  * @Author: 一尾流莺
  * @Description:过滤数组中的唯一值
  * @Date: 2021-09-15 10:54:18
- * @LastEditTime: 2021-09-15 10:59:50
+ * @LastEditTime: 2021-09-15 11:13:07
  * @FilePath: \warblerJS\src\array\filterUnique.js
  */
 
@@ -16,7 +16,7 @@ const filterUnique = (arr) => arr.filter((i) => arr.indexOf(i) !== arr.lastIndex
 /** 用法
  const arr = [1,2,2,3,3,4,5,5,6]
  const result = filterUnique(arr)
- console.log(result) => [2,2,3,3,5,5]
+ console.log(result) //=> [2,2,3,3,5,5]
  */
 
 export default filterUnique;
