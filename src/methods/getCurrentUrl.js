39336b9f4404b5c3e8c1f61d922ded2fef4f28ce
韩志伟 <hanzhiwei@lucidata.cn>
2021-09-17 17:53:53 +0800

    版本升级1.0.7

--- src/methods/getCurrentUrl.js
+++ src/methods/getCurrentUrl.js
@@ -2,14 +2,14 @@
  * @Author: 一尾流莺
  * @Description:获取当前页面的URL
  * @Date: 2021-09-17 15:52:02
- * @LastEditTime: 2021-09-17 15:59:02
+ * @LastEditTime: 2021-09-17 17:31:16
  * @FilePath: \warblerJS\src\methods\getCurrentUrl.js
  */
 
 /**
  * @description:获取当前页面的URL
  * @param {*} null
- * @return {*} 当前页面的URL
+ * @return { String } 当前页面的URL
  */
 const getCurrentUrl = () => window.location.href;
 
