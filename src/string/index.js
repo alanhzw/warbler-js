98e1c944c168814d1885307fb69999e00c306235
韩志伟 <hanzhiwei@lucidata.cn>
2021-10-13 11:45:40 +0800

    string

--- src/string/index.js
+++ src/string/index.js
@@ -2,24 +2,24 @@
  * @Author: 一尾流莺
  * @Description:操作字符串相关的方法
  * @Date: 2021-09-10 11:15:49
- * @LastEditTime: 2021-09-10 14:59:49
+ * @LastEditTime: 2021-10-13 11:44:52
  * @FilePath: \warblerJS\src\string\index.js
  */
 
 // 反转字符串
-import reverseString from './reverseString';
+import reverseString from './other/reverseString';
 
 // 将字符串的首字母转换成大写字母
-import toUpperFirstLetter from './toUpperFirstLetter';
+import toUpperFirstLetter from './exchange/toUpperFirstLetter';
 
 // 将字符串的首字母转换成大写字母
-import toLowerFirstLetter from './toLowerFirstLetter';
+import toLowerFirstLetter from './exchange/toLowerFirstLetter';
 
 // 将字符串中每个单词的首字母转为大写
-import capitalizeWord from './capitalizeWord';
+import capitalizeWord from './exchange/capitalizeWord';
 
 // 将字符串按字母顺序排列
-import alphabetical from './alphabetical';
+import alphabetical from './sort/alphabetical';
 
 export {
   reverseString,
