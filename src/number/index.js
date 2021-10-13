782cc0a43ce201e915b667a32accb7239bcada0d
韩志伟 <hanzhiwei@lucidata.cn>
2021-10-13 11:43:28 +0800

    number

--- src/number/index.js
+++ src/number/index.js
@@ -2,24 +2,24 @@
  * @Author: 一尾流莺
  * @Description:操作数字相关的方法
  * @Date: 2021-09-10 11:15:38
- * @LastEditTime: 2021-09-15 11:01:15
+ * @LastEditTime: 2021-10-13 11:43:10
  * @FilePath: \warblerJS\src\number\index.js
  */
 
 // 判断一个数字是偶数还是奇数
-import isEvenNumber from './isEvenNumber';
+import isEvenNumber from './judge/isEvenNumber';
 
 // 获取参数的平均数值
 import getAverage from './getAverage';
 
 // 判断数字是否可以整除
-import isDivisible from './isDivisible';
+import isDivisible from './judge/isDivisible';
 
 // 生成范围内的随机整数
-import randomIntegerInRange from './randomIntegerInRange';
+import randomIntegerInRange from './random/randomIntegerInRange';
 
 // 生成范围内的随机数
-import randomInRange from './randomInRange';
+import randomInRange from './random/randomInRange';
 
 export {
   isEvenNumber,
