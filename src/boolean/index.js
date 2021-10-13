a3edf39b136a376258a5d9ec50ad2081aefc22f2
韩志伟 <hanzhiwei@lucidata.cn>
2021-10-13 11:48:30 +0800

    methods

--- src/boolean/index.js
+++ src/boolean/index.js
@@ -2,12 +2,12 @@
  * @Author: 一尾流莺
  * @Description:
  * @Date: 2021-09-10 14:52:45
- * @LastEditTime: 2021-09-10 14:52:45
+ * @LastEditTime: 2021-10-13 11:46:05
  * @FilePath: \warblerJS\src\boolean\index.js
  */
 
 // 获得一个随机的布尔值（true/false）
-import randomBoolean from './randomBoolean';
+import randomBoolean from './other/randomBoolean';
 
 export {
   randomBoolean,
--- src/methods/index.js
+++ src/methods/index.js
@@ -2,21 +2,21 @@
  * @Author: 一尾流莺
  * @Description:常用方法
  * @Date: 2021-09-10 11:44:18
- * @LastEditTime: 2021-09-14 15:36:00
- * @FilePath: \warblerJS\src\methods\index.js
+ * @LastEditTime: 2021-10-13 11:47:28
+ * @FilePath: \warblerJS\src\methods\other\index.js
  */
 
 // 模拟延迟
-import imitateDelay from './imitateDelay';
+import imitateDelay from './other/imitateDelay';
 
 // 异步加载script脚本
-import loadScript from './loadScript';
+import loadScript from './other/loadScript';
 
 // 获取当前页面的URL
-import getCurrentUrl from './getCurrentUrl';
+import getCurrentUrl from './other/getCurrentUrl';
 
 // 滚动到页面顶部
-import scrollToTop from './scrollToTop';
+import scrollToTop from './other/scrollToTop';
 
 export {
   imitateDelay,
